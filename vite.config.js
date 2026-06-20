5718094753e5d2bd770f274a716c05bf43abc115
Lucas Morales <lucasvmorales@protonmail.com>
2026-06-20 17:09:32 -0300

    ci: adding github-pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: repoName ? `/${repoName}/` : '/',
   plugins: [tailwindcss(), vue()],
 })
